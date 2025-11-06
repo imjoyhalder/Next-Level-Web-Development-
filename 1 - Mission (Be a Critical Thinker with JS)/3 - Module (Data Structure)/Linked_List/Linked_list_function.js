8eda53088c2e1bb51190673a76a76c5fc34dfece
imjoyhalder <joyhalder00113355@gmail.com>
2025-11-07 02:49:16 +0600

    insert function added into Linked List

--- 1 - Mission (Be a Critical Thinker with JS)/3 - Module (Data Structure)/Linked_List/Linked_list_function.js
+++ 1 - Mission (Be a Critical Thinker with JS)/3 - Module (Data Structure)/Linked_List/Linked_list_function.js
@@ -27,15 +27,54 @@ class LinkedList {
 
     prepend(value) {
         const newNode = new Node(value)
-        if(this.head===null){
+        if (this.head === null) {
             this.head = newNode
             this.tail = newNode
         }
-        else{
+        else {
             newNode.next = this.head
-            this.head =  newNode
+            this.head = newNode
         }
         this.length++
+    }
+
+    insert(index, value) {
+
+        if (index < 0 || index > this.length) {
+            console.log('Index out of bound');
+            return
+        }
+
+        if (index == 0) {
+            this.prepend(value)
+            this.length++
+            return
+        }
+        if (index == this.length) {
+            this.append(value)
+            this.length++
+            return
+        }
+
+        let temp = this._traverseOfIndex(index)
+
+        const newNode = new Node(value)
+        let oldNode = temp.next
+        newNode.next = oldNode
+        temp.next = newNode
+
+        this.length++
+    }
+
+    _traverseOfIndex(index) {
+        let temp = this.head
+        let count = 0
+
+        while (count != index - 1) {
+            temp = temp.next
+            count++
+        }
+        return temp
     }
 
     print() {
@@ -46,7 +85,7 @@ class LinkedList {
             arr.push(temp.value)
             temp = temp.next
         }
-        console.log(arr.join(' --> ',' -->null'));
+        console.log(arr.join(' --> ', ' -->null'));
     }
 
     isEmpty() {
@@ -58,7 +97,7 @@ class LinkedList {
     }
 
     peekHead() {
-        if(this.head){
+        if (this.head) {
             console.log('Empty')
         }
         console.log(this.head.value);
@@ -68,8 +107,10 @@ class LinkedList {
 const lnkList = new LinkedList()
 lnkList.append(10)
 lnkList.append(20)
-lnkList.prepend(100)
-lnkList.prepend(200)
-console.log(lnkList.getLength);
+lnkList.append(30)
+lnkList.append(40)
+lnkList.insert(3, 1000)
+
+
 lnkList.print()
 
