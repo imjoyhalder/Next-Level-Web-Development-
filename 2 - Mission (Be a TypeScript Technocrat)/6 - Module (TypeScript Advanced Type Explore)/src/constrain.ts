0334f0b99d761e17c7c63aebd2f3e51ba02890d5
imjoyhalder <joyhalder00113355@gmail.com>
2025-11-14 02:19:00 +0600

    constrain with dynamic type

---
+++ 2 - Mission (Be a TypeScript Technocrat)/6 - Module (TypeScript Advanced Type Explore)/src/constrain.ts
@@ -0,0 +1,26 @@
+// set constraint with dynamic type or generic 
+type Student = {
+    id: number,
+    name: string
+}
+
+const addStudentToCourse = <T extends Student>(studentInfo: T) => {
+    return {
+        course: "Next Level",
+        ...studentInfo
+    }
+}
+
+const student1 = {
+    id: 123,
+    name: "Mahabub",
+    hasCar: false
+}
+const student2 = {
+    hasWatch: false,
+    id: 123,
+    name: "Mahabub",
+}
+
+console.log(addStudentToCourse(student1));
+addStudentToCourse(student2)
