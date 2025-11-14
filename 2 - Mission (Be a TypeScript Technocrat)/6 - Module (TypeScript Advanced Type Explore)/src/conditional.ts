ba97df9ca5ae423dee7a12142231db7d5f64c879
imjoyhalder <joyhalder00113355@gmail.com>
2025-11-15 02:00:22 +0600

    conditional type added

---
+++ 2 - Mission (Be a TypeScript Technocrat)/6 - Module (TypeScript Advanced Type Explore)/src/conditional.ts
@@ -0,0 +1,15 @@
+type A = null
+type B = undefined
+
+type C = A extends null ? true : B extends undefined ? true : false; 
+
+type RichPeoplesVehicle = {
+    bike: string; 
+    car: string; 
+    ship: string; 
+}
+
+type CheckVehicle<T> = T extends keyof RichPeoplesVehicle ? true: false; 
+
+
+type HasBike = CheckVehicle<"bike">
