c8cfc58ba73b5a38f90889788e971a4e4913133e
imjoyhalder <joyhalder00113355@gmail.com>
2025-11-25 22:55:28 +0600

    file operation codes added

--- 2 - Mission (Be a TypeScript Technocrat)/10 - Module (Deep Dive into Node.js Core Modules)/write-file-sync.js
+++ 2 - Mission (Be a TypeScript Technocrat)/10 - Module (Deep Dive into Node.js Core Modules)/write-file-sync.js
@@ -1,3 +1,20 @@
+const { error } = require('console');
 const fs = require('fs')
 
 const content1  = "Hello man how are you  . do you know about node js"
+try {
+    fs.writeFileSync('./Output/test-async.txt',content1)
+    console.log('File write async');
+} catch (error) {
+    console.log(error.message);
+}
+
+const content2 = "This is async too"
+fs.writeFile('./Output/test-async.txt',content2,(error)=>{
+    if(error){
+        console.log(error.message);
+    }
+    else{
+        console.log('File written successfully');
+    }
+})
