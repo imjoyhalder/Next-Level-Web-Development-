d00863857be527add1e12affc2920c9c70628301
imjoyhalder <joyhalder00113355@gmail.com>
2025-11-29 02:35:52 +0600

    build api using node.js and typescript

--- 3 - Mission (Be a Node Express Expert)/11 -Module (Building Server with Raw Node.js and Typescript )/src/server.ts
+++ 3 - Mission (Be a Node Express Expert)/11 -Module (Building Server with Raw Node.js and Typescript )/src/server.ts
@@ -1,17 +1,28 @@
+
 import http, { IncomingMessage, Server, ServerResponse } from 'http'
 import path from 'path';
+import config from './config';
 
-const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse )=>{
+const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
     console.log(`Server is running .......`);
-    if(req.url == '/' && req.method == "GET"){
-        res.writeHead(200, {'content-type':'application/json'})
+    if (req.url == '/' && req.method == "GET") {
+        res.writeHead(200, { 'content-type': 'application/json' })
         res.end(JSON.stringify({
-            message: 'Hello from node js with TypeScript...', 
+            message: 'Hello from node js with TypeScript...',
             path: req.url
         }))
     }
-}) 
 
-server.listen(5000, ()=>{
+    if (req.url == '/api' && req.method == "GET") {
+        
+        res.writeHead(200, { 'content-type': 'application/json' })
+        res.end(JSON.stringify({
+            message: 'Health status ok...',
+            path: req.url
+        }))
+    }
+})
+
+server.listen(config.port, () => {
     console.log(`Server is running on port: ${5000}`);
 })
