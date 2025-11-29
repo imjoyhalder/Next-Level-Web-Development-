e4a247fe1bd44ba96f750dd79d0144db9f7d055d
imjoyhalder <joyhalder00113355@gmail.com>
2025-11-29 23:23:48 +0600

    make custom router handler

--- 3 - Mission (Be a Node Express Expert)/11 -Module (Building Server with Raw Node.js and Typescript )/src/server.ts
+++ 3 - Mission (Be a Node Express Expert)/11 -Module (Building Server with Raw Node.js and Typescript )/src/server.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import config from './config';
 
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    console.log(`Server is running .......`);
+    // console.log(`Server is running .......`);
     if (req.url == '/' && req.method == "GET") {
         res.writeHead(200, { 'content-type': 'application/json' })
         res.end(JSON.stringify({
@@ -14,13 +14,33 @@ const server: Server = http.createServer((req: IncomingMessage, res: ServerRespo
     }
 
     if (req.url == '/api' && req.method == "GET") {
-        
+
         res.writeHead(200, { 'content-type': 'application/json' })
         res.end(JSON.stringify({
             message: 'Health status ok...',
             path: req.url
         }))
     }
+
+    if (req.url == '/api/users' && req.method == "POST") {
+        let body = ''
+
+        req.on('data', (chunk) => {
+            body += chunk
+        })
+
+        req.on('end', () => {
+            try {
+                const pareBody = JSON.parse(body);
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                console.log(pareBody);
+                res.end(JSON.stringify(pareBody))
+            } catch (error:any) {
+                console.log(error?.message);
+            }
+        })
+    }
+
 })
 
 server.listen(config.port, () => {
