542f71c37686cec3b31d1e8594cc141961ad2f9b
imjoyhalder <joyhalder00113355@gmail.com>
2025-11-30 04:33:43 +0600

    user delete method dynamic route create

--- 3 - Mission (Be a Node Express Expert)/11 -Module (Building Server with Raw Node.js and Typescript )/src/routes/index.ts
+++ 3 - Mission (Be a Node Express Expert)/11 -Module (Building Server with Raw Node.js and Typescript )/src/routes/index.ts
@@ -33,3 +33,48 @@ addRoutes("POST", '/api/users', async(req, res) => {
         console.log(error.message);
     }
 })
+
+
+addRoutes("PUT", '/api/users/:id', async(req, res)=>{
+    const {id} = (req as any).params
+    const body = await paresBody(req)
+
+    const users = readUsers()
+    
+    const index = users.findIndex((user: any)=> user.id == id)
+    if(index===-1){
+        sendJson(res, 404, {
+            success: false, 
+            message: 'user not found'
+        })
+    }
+    users[index] = {
+        ...users[index], ...body
+    }
+    writeUsers(users)
+    sendJson(res, 202, {
+        success:true, 
+        message: `${id} user updated`, 
+        data: users[index]
+    })
+})
+
+addRoutes("DELETE", '/api/users/:id', async(req, res)=>{
+    const {id} = (req as any).params
+
+    const users = readUsers()
+    const index = users.findIndex((user: any)=> user.id == id)
+    if(index===-1){
+        sendJson(res, 404, {
+            success: false, 
+            message: `${id} user not found` 
+        })
+    }
+    users.splice(index, 1)
+    // users[index] = {}
+    writeUsers(users)
+    sendJson(res,200,{
+        success: true, 
+        message: `${id} user deleted`
+    })
+})
--- 3 - Mission (Be a Node Express Expert)/11 -Module (Building Server with Raw Node.js and Typescript )/src/server.ts
+++ 3 - Mission (Be a Node Express Expert)/11 -Module (Building Server with Raw Node.js and Typescript )/src/server.ts
@@ -1,38 +1,9 @@
-
 import http, { IncomingMessage, Server, ServerResponse } from 'http'
 import path from 'path';
 import config from './config';
 import { RouteHandler, routes } from './helpers/RouteHandler';
 import './routes'
-
-const findDynamicRoute = (method: string, url: string)=>{
-    const methodMap = routes.get(method)
-    if(!methodMap) return null; 
-
-    for(const[routePath, handler] of methodMap.entries()){
-        const routeParts = routePath.split('/')
-        const urlParts = url.split('/')
-
-        if(routeParts.length !== urlParts.length) continue; 
-
-        const params:any = {}
-        let matched = true
-
-        for(let i= 0; i<routeParts.length; i++){
-            if(routeParts[i]?.startsWith(':')){
-                params[routeParts[i]?.substring(1)!] = urlParts[i]
-            }
-            else if(routeParts[i]!== urlParts[i]){
-                matched = false; 
-                break; 
-            }
-        }
-        if(matched){
-            return {handler, params}
-        }
-    }
-    return null; 
-}
+import { findDynamicRoute } from './helpers/dynamicRoutes';
 
 
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
