063b146f18fee9b249e9492436fd45bbe0d2c74b
imjoyhalder <joyhalder00113355@gmail.com>
2025-12-01 03:49:08 +0600

    products put  method created

--- 3 - Mission (Be a Node Express Expert)/Conceptual-Session (Raw Nodejs Routing and CRUD)/routing-and-crud/src/controller/products.controller.ts
+++ 3 - Mission (Be a Node Express Expert)/Conceptual-Session (Raw Nodejs Routing and CRUD)/routing-and-crud/src/controller/products.controller.ts
@@ -3,7 +3,7 @@ import { readProducts, writeProduct } from "../services/products.service";
 import { IProduct } from "../types/Iporduct.interface";
 import { parseBody } from "../utility/parseBody";
 
-export const productController = async(req: IncomingMessage, res: ServerResponse) => {
+export const productController = async (req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
     const method = req.method;
 
@@ -15,23 +15,23 @@ export const productController = async(req: IncomingMessage, res: ServerResponse
         const products = readProducts()
         res.writeHead(200, { 'content-type': "application/json" })
         res.end(JSON.stringify({ success: true, path: url, data: products }))
-        return; 
+        return;
     }
     else if (method === 'GET' && id !== null) {
         const products = readProducts()
-        const product = products.find((p: IProduct)=> p.id === id)
+        const product = products.find((p: IProduct) => p.id === id)
 
         res.writeHead(200, { 'content-type': "application/json" })
         res.end(JSON.stringify({ success: true, data: product }))
-        return; 
+        return;
     }
 
-    else if(method==='POST' && url==='/products'){
+    else if (method === 'POST' && url === '/products') {
         const body = await parseBody(req)
-        const products = readProducts(); 
+        const products = readProducts();
 
         const newProduct = {
-            id: Date.now(), 
+            id: Date.now(),
             ...body
         }
 
@@ -41,7 +41,28 @@ export const productController = async(req: IncomingMessage, res: ServerResponse
 
         res.writeHead(200, { 'content-type': "application/json" })
         res.end(JSON.stringify({ success: true, data: newProduct }))
-        return; 
+        return;
+    }
+
+    else if (method === "PUT" && id != null) {
+        const body = await parseBody(req)
+        const products = readProducts();
+
+        const index = products.findIndex((p: IProduct) => p.id == id)
+        if (index < 0) {
+            res.writeHead(201, { "content-type": 'application/json' })
+            res.end(
+                JSON.stringify({
+                    message: 'Products not found'
+                })
+            )
+            return;
+        }
+        products[index] = { id: products[index].id, ...body }
+        writeProduct(products)
+        res.writeHead(200, { 'content-type': "application/json" })
+        res.end(JSON.stringify({ message: 'Product updated successfully', data: products[index]}))
+        return;
     }
 
 }
