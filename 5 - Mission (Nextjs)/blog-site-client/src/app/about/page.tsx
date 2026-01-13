268841d06d564f322491cb352515349dcc5faa4f
imjoyhalder <joyhalder00113355@gmail.com>
2026-01-14 02:57:48 +0600

    added multiple file

---
+++ 5 - Mission (Nextjs)/blog-site-client/src/app/about/page.tsx
@@ -0,0 +1,6 @@
+
+export default function AboutPage() {
+    return (
+        <div className="text-4xl">this is the about page</div>
+    )
+}
---
+++ 5 - Mission (Nextjs)/blog-site-client/src/app/contact/branch/page.tsx
@@ -0,0 +1,10 @@
+
+const BranchPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BranchPage;
