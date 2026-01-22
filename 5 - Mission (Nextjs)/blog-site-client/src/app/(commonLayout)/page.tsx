3f0a37ab57fd1ff474d1eaba203777960ba040a2
imjoyhalder <joyhalder00113355@gmail.com>
2026-01-23 04:10:23 +0600

    update into multiple file

--- 5 - Mission (Nextjs)/blog-site-client/src/app/(commonLayout)/page.tsx
+++ 5 - Mission (Nextjs)/blog-site-client/src/app/(commonLayout)/page.tsx
@@ -2,11 +2,14 @@ import BlogCard from "@/components/modules/home-page/BlogCard";
 import { blogService } from "@/services/blog.service";
 import { BlogPost } from "@/types";
 
+
 export default async function Home() {
 
-  const { data } = await blogService.getBlogPost()
+  const { data } = await blogService.getBlogPost(
+    {isFeatured: false, search: ""}, 
+    {revalidate: 10})   
 
-  console.log(data);
+    //cache: "no-store",
   return (
     <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5 md:px-8 px-2 mt-2">
       {
--- 5 - Mission (Nextjs)/blog-site-client/src/services/blog.service.ts
+++ 5 - Mission (Nextjs)/blog-site-client/src/services/blog.service.ts
@@ -1,18 +1,55 @@
+import { config } from './../proxy';
+import { cache } from 'react';
 
-import{ env} from '../env'
+
+import { env } from '../env'
 
 const API_URL = env.API_URL
 
+interface ServiceOptions {
+    cache?: RequestCache
+    revalidate?: number
+}
+interface GetBlogParams {
+    isFeatured?: boolean
+    search?: string
+}
+
 export const blogService = {
-    getBlogPost: async () => {
+    getBlogPost: async (params?: GetBlogParams, options?: ServiceOptions ) => {
         try {
-            const res =  await fetch(`${API_URL}/posts`, {next:{revalidate: 10}})
+
+            const url = new URL(`${API_URL}/posts`)
+            // url.searchParams.append("key", "value")
+
+            if (params) {
+                // console.log(Object.entries(params));
+                Object.entries(params).forEach(([key, value]) => {
+                    if (value !== null && value !== undefined && value !== "") {
+                        url.searchParams.append(key, value)
+                    }
+                })
+            }
+
+            console.log(url.toString());
+
+            const config: RequestInit = {}
+
+            if(options?.cache){
+                config.cache = options.cache
+            }
+
+            if(options?.revalidate){
+                config.next = {revalidate: options.revalidate}
+            }
+
+            const res = await fetch(url.toString(), config)
             const data = await res.json()
 
-            return {data: data, error: null}
+            return { data: data, error: null }
 
         } catch (error) {
-            return {data: null, error: {message: "Something went wrong"}}
+            return { data: null, error: { message: error } }
         }
     }
 }
