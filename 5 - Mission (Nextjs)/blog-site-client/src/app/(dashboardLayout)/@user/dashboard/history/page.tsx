cf579146983ac9dee7c5cd70f7ee9228b6902ad2
imjoyhalder <joyhalder00113355@gmail.com>
2026-01-27 03:55:21 +0600

    update ui

--- 5 - Mission (Nextjs)/blog-site-client/src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
+++ 5 - Mission (Nextjs)/blog-site-client/src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
@@ -1,18 +1,28 @@
 import HistoryTable from '@/components/modules/user/history/HistoryTable';
+import PaginationControls from '@/components/ui/pagination-controls';
 import { blogService } from '@/services/blog.service';
 import React from 'react';
 
 const HistoryPage = async ({ searchParams }: { searchParams: Promise<{ page: string }> }) => {
 
     const { page } = await searchParams
-    console.log(page);
+    
 
     const response = await blogService.getBlogPost({ page })
     const posts = response.data?.data || []
+
+    const pagination = response.data?.pagination || {
+        limit: 10, 
+        page: 1, 
+        total: 0, 
+        totalPage: 1
+    }
+
     return (
         <div className='p-6'>
             <h1 className='text-2xl font-bold mb-6'>Blog post history</h1>
             <HistoryTable posts={posts} />
+            <PaginationControls meta={pagination}/>
         </div>
     );
 };
